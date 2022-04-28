183191255893741e249daa6844b5451e80bb9d85
yezee.choi <almightyj12@naver.com>
2022-04-29 00:12:25 +0900

    ui manager 2.1 healthbar

--- sorce/UI/HealthBar.cpp
+++ sorce/UI/HealthBar.cpp
@@ -8,15 +8,15 @@ HealthBar::HealthBar()
 
 void HealthBar::Init(Vector2i position, int health)
 {
-	float healthBarWidth = health*5;
+	float healthBarWidth = health * 3;
 	float healthBarHeight = 30;
 
 	Vector2f healthBarSize = Vector2f(healthBarWidth, healthBarHeight);
 	rectShape.setSize(healthBarSize);
-	FloatRect healthRect = rectShape.getLocalBounds();
-
-	Vector2f healthBarPos = Vector2f(position.x * 0.5f - healthBarWidth * 0.5f, position.y * 0.8f);
-	rectShape.setPosition(healthBarPos);
+	//FloatRect textRect = rectShape.getLocalBounds();
+	//rectShape.setOrigin(
+	//	textRect.left, textRect.top + textRect.height * 0.5f);
+	rectShape.setPosition(position.x * 0.35f, position.y * 0.8f);
 	rectShape.setFillColor(Color::Red);
 
 }
