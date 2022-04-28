fd871bb2c6869c722419a0b928276db399a74172
yezee.choi <almightyj12@naver.com>
2022-04-28 15:54:44 +0900

    ui Manager 1.0

---
+++ sorce/UI/UIManager.cpp
@@ -0,0 +1,92 @@
+#include "UIManager.h"
+#include <iostream>
+#include <sstream>
+#include "../player/Player.h"
+
+UI::UI()
+	: curBullets(START_COUNT_OF_BULLETS), wave(START_WAVE)
+{
+	font.loadFromFile("fonts/zombiecontrol.ttf");
+	textScore.setFont(font);
+	textScore.setFillColor(Color::White);
+	textScore.setCharacterSize(60);
+
+	textHighScore.setFont(font);
+	textHighScore.setFillColor(Color::White);
+	textHighScore.setCharacterSize(60);
+
+	zombieCount.setFont(font);
+	zombieCount.setFillColor(Color::White);
+	zombieCount.setCharacterSize(50);
+
+	waveNum.setFont(font);
+	waveNum.setFillColor(Color::White);
+	waveNum.setCharacterSize(50);
+
+	textBullet.setFont(font);
+	textBullet.setFillColor(Color::White);
+	textBullet.setCharacterSize(50);
+	FloatRect textRect = textBullet.getLocalBounds();
+	textBullet.setOrigin(
+		textRect.left + textRect.width * 0.5f,
+		textRect.top + textRect.height * 0.5f
+	);
+	texture.loadFromFile("graphics/ammo_icon.png");
+}
+
+void UI::Update(int score, int health, int numOfZombies, Vector2f position)
+{
+
+	//--------------점수--------------
+	stringstream ss;
+	ss << "Score = " << score;
+	textScore.setString(ss.str());
+	textScore.setPosition(position.x * 0.18f, position.y * 0.1f);
+
+	stringstream hs;
+	hs << "High Score = " << score;
+	textHighScore.setString(hs.str());
+	textHighScore.setPosition(position.x * 0.7f, position.y * 0.1f);
+
+	//------------좀비카운트------------
+	stringstream zs;
+	zs << "Zombies : " << numOfZombies;
+	zombieCount.setString(zs.str());
+	zombieCount.setPosition(position.x * 0.8f, position.y * 0.8f);
+
+	//--------------웨이브--------------
+	stringstream ws;
+	ws << "Wave : " << wave;
+	waveNum.setString(ws.str());
+	waveNum.setPosition(position.x * 0.65f, position.y * 0.8f);
+
+	//--------------잔탄수--------------
+	sprite.setTexture(texture);
+	sprite.setPosition(position.x * 0.18f, position.y * 0.8f);
+
+	stringstream bs;
+	bs << curBullets << "/" << START_COUNT_OF_BULLETS;
+	textBullet.setString(bs.str());
+	textBullet.setPosition(position.x * 0.22f, position.y * 0.8f);
+
+
+	//-------------체력바-------------
+	float healthBarWidth = 200;
+	float healthBarHeight = 30;
+	Vector2f healthBarSize = Vector2f(healthBarWidth, healthBarHeight);
+	rectShape.setSize(healthBarSize);
+	Vector2f healthBarPos = Vector2f(position.x * 0.5f - healthBarWidth * 0.5f, position.y * 0.8f);
+	rectShape.setPosition(healthBarPos);
+	rectShape.setFillColor(Color::Red);
+}
+
+void UI::Draw(RenderWindow& window)
+{
+	window.draw(textScore);
+	window.draw(textHighScore);
+	window.draw(zombieCount);
+	window.draw(waveNum);
+	window.draw(sprite);
+	window.draw(textBullet);
+	window.draw(rectShape);
+}
---
+++ sorce/UI/UIManager.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+class Player;
+
+class UI
+{
+private:
+	const float START_COUNT_OF_BULLETS = 24;
+	const float START_WAVE = 0;
+
+	Font font;
+	Text textScore;
+	Text textBullet;
+	Text textHighScore;
+	Text zombieCount;
+	Text waveNum;
+	RectangleShape rectShape;
+	Texture texture;
+	Sprite sprite;
+
+	int numOfZombies;
+	int score;
+	int highScore;
+	int wave;
+
+	int curBullets;
+public:
+	UI();
+	void Update(int score, int health, int numOfZombies, Vector2f position);
+	void Draw(RenderWindow& window);
+};
+
